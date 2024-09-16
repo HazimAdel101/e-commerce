7feeca98b4d92c43bda9e2da98124cd0b15a9654
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2024-09-16 14:47:11 +0300

    update the user fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'photo',
+        'phone',
+        'role',
+        'status',
+        'address',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('photo');
+            $table->string('phone');
+            $table->text('address');
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('status', ['acticve', 'inacticve'])->default('acticve');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
